106a07845d9a45cac701a03b17b432a5e9bbd6ae
MDJ <Jrakastill@hotmail.com>
2024-06-10 01:37:52 +1000

    Part 3 of HGW (Grow).js

--- Part 3 of HGW (Grow).js
+++ Part 3 of HGW (Grow).js
@@ -1,1 +1,15 @@
+/** @param {NS} ns */
+export async function main(ns) {
 
+  const target = "harakiri-sushi";
+
+  const moneyThresh = ns.getServerMaxMoney(target);
+
+While(true); {
+  if (ns.getServerMoneyAvailable(Target) < moneyThresh) {
+   await ns.grow(Target) }
+  else { 
+    await ns.sleep(1000); 
+   }
+ }
+}
