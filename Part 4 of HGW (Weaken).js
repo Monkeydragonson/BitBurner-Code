4edceb957405fe3d780af7d1ab060c3aeb158d53
MDJ <Jrakastill@hotmail.com>
2024-06-10 01:02:05 +1000

    Part 4 of HGW (Weaken).js

---
+++ Part 4 of HGW (Weaken).js
@@ -0,0 +1,18 @@
+/** @param {NS} ns */
+export async function main(ns) {
+
+  const target = "harakiri-sushi";
+
+  const securityThresh = ns.getServerMinSecurityLevel(target);
+
+  while (true) {
+    if (ns.getServerSecurityLevel(target) > securityThresh) {
+ // If the server's security level is above our threshold, weaken it
+      await ns.weaken(target);
+    }
+    else {
+      await ns.sleep(1000)
+  // Otherwise sleep
+    }
+  }
+}
