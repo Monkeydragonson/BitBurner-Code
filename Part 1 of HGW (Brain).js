d8bab7453c5fca6cdb16d4a967b09dc8a610100a
MDJ <Jrakastill@hotmail.com>
2024-06-10 01:20:37 +1000

    Part 1 of HGW (Brain).js

--- Part 1 of HGW (Brain).js
+++ Part 1 of HGW (Brain).js
@@ -1,1 +1,6 @@
+//this is the link to a moeny% per hack thread equation thing.
+//https://www.reddit.com/r/Bitburner/comments/tt8mi1/how_to_calculate_ratios_of_hacking_scripts/
+// growthanalyze(target, maxServerMoney / (maxServerMoney - totalAmtHacked)) is important
 
+//hackanalyze returns the percentage that will be hacked with one thread at the current security level.
+//try this  growthAnalyze(servers[i],1/(1-hackAnalyze(servers[i]))) (thanks u/creeper_the_cat on reddit)
