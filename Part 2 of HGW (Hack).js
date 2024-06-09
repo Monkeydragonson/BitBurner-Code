1525edc735e9e637624fedd6461e620cda8b0f0c
MDJ <Jrakastill@hotmail.com>
2024-06-10 01:14:15 +1000

    Part 2 of HGW (Hack).js

--- Part 2 of HGW (Hack).js
+++ Part 2 of HGW (Hack).js
@@ -1,1 +1,16 @@
+/** @param {NS} ns */
+export async function main(ns) {
 
+  const target = "harakiri-sushi";
+
+  const moneyThresh = ns.getServerMaxMoney(target)
+
+  while (true) {
+    if (ns.getServerMoneyAvailable(target) < moneyThresh) {
+      await ns.sleep(1000)
+    } else {
+      await ns.hack(target);
+
+    }
+  }
+}
