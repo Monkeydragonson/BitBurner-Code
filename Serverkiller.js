f0a7781ea4795531baaba9ef4aa6f5caf8bffa86
MDJ <Jrakastill@hotmail.com>
2024-06-10 00:52:24 +1000

    Serverkiller.js

---
+++ Serverkiller.js
@@ -0,0 +1,30 @@
+/** @param {NS} ns */
+export async function main(ns) {
+
+ns.deleteServer("pserv-0");
+ns.deleteServer("pserv-1");
+ns.deleteServer("pserv-2");
+ns.deleteServer("pserv-3");
+ns.deleteServer("pserv-4");
+ns.deleteServer("pserv-5");
+ns.deleteServer("pserv-6");
+ns.deleteServer("pserv-7");
+ns.deleteServer("pserv-8");
+ns.deleteServer("pserv-9");
+ns.deleteServer("pserv-10");
+ns.deleteServer("pserv-11");
+ns.deleteServer("pserv-12");
+ns.deleteServer("pserv-13");
+ns.deleteServer("pserv-14");
+ns.deleteServer("pserv-15");
+ns.deleteServer("pserv-16");
+ns.deleteServer("pserv-17");
+ns.deleteServer("pserv-18");
+ns.deleteServer("pserv-19");
+ns.deleteServer("pserv-20");
+ns.deleteServer("pserv-21");
+ns.deleteServer("pserv-22");
+ns.deleteServer("pserv-23");
+ns.deleteServer("pserv-24")
+
+}
