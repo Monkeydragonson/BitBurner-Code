f8b32d02a85db96e1ea2ca8542ba883d95f69cfa
MDJ <Jrakastill@hotmail.com>
2024-06-11 14:50:49 +1000

    Part 4 of HGW (Weaken).js

--- Part 4 of HGW (Weaken).js
+++ Part 4 of HGW (Weaken).js
@@ -5,14 +5,9 @@ export async function main(ns) {
 
   const securityThresh = ns.getServerMinSecurityLevel(target);
 
-  while (true) {
     if (ns.getServerSecurityLevel(target) > securityThresh) {
  // If the server's security level is above our threshold, weaken it
       await ns.weaken(target);
-    }
-    else {
-      await ns.sleep(1000);
-  // Otherwise sleep
     }
   }
 }
